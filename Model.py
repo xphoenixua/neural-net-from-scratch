adf91cc313d613044d8470a91afc48626f12489e
xphoenixua <uzya03@gmail.com>
2023-11-05 19:48:03 +0200

    save the best model; evaluation code improvements

--- Model.py
+++ Model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import copy
 
 class Model:
     '''
@@ -42,23 +43,31 @@ class Model:
             n_epochs : int (number of epochs for training)
             learning_rate : float (learning rate for optimization)
         '''
-        losses = []
+        best_loss_ = np.inf
+        self.losses_ = []
         for epoch in range(n_epochs):
-            Y_hat = X_train # the input for the first hidden layer
+            Y_pred = X_train # the input for the first hidden layer
+            np.random.shuffle(Y_pred) # shuffle samples to prevent order learned biases
             for layer in self.layers:
-                Y_hat = layer.forward_pass(Y_hat)
+                Y_pred = layer.forward_pass(Y_pred)
+
             # loss after forward passing all layers
-            loss_value = self.loss(Y_train, Y_hat)
-            losses.append(loss_value)
-            
-            # calculating dL/dY_hat derivative
-            error = self.loss_derivative(Y_train, Y_hat)
+            loss_value = self.loss(Y_train, Y_pred)
+            self.losses_.append(loss_value)
+            if loss_value < best_loss_:
+                best_loss = loss_value
+                best_model_ = copy.deepcopy(self.layers)
+
+            # calculating dL/dY_pred derivative
+            error = self.loss_derivative(Y_train, Y_pred)
             for layer in self.layers[::-1]:
                 # print(f'epoch:{epoch}, layer:{layer}')
                 error = layer.backward_pass(error, learning_rate, clip_norm)
-        
+
             if epoch % 100 == 0:
                 print(f'Epoch {epoch}, Loss: {loss_value:.4f}')
+
+        self.layers = best_model_
 
     def predict(self, X_val):
         '''
@@ -75,24 +84,22 @@ class Model:
             Y_val = layer.forward_pass(Y_val) # update output each forward pass
         return Y_val # final predicitons
     
-    def calculate_confusion_matrix(self, Y, Y_hat):
+    def calculate_confusion_matrix(self, Y, Y_pred):
         '''
         Calculate the confusion matrix for the given true labels and predictions.
 
         Parameters:
             Y : ndarray (true labels)
-            Y_hat : ndarray (predicted labels)
+            Y_pred : ndarray (predicted labels)
 
         Returns:
             c_matrix : ndarray (the confusion matrix as a 2D numpy array)
         '''
         k = np.unique(Y).shape[0] # number of classes
         c_matrix = np.zeros((k,k))
-        if k == 2:
-            Y_hat = (Y_hat >= 0.5).astype(int)
-        print('\n', pd.DataFrame(np.hstack([Y, Y_hat]), columns=['true', 'pred']))
+        print('\n', pd.DataFrame(np.hstack([Y, Y_pred]), columns=['true', 'pred']))
         for i in range(Y.shape[0]):
-            c_matrix[Y[i], Y_hat[i]] += 1
+            c_matrix[Y[i], Y_pred[i]] += 1
         return c_matrix
 
     def evaluate(self, y_val, predictions):
@@ -107,24 +114,19 @@ class Model:
             results : list (list containing the evaluation metrics results for each class)
         '''
         c_matrix = self.calculate_confusion_matrix(y_val, predictions)
-        print('\n', pd.DataFrame(c_matrix, columns=[0, 1], index=[0, 1]))
         results = []
-        accuracy = 0
+        accuracy = np.trace(c_matrix) / np.sum(c_matrix)
         for i in range(c_matrix.shape[0]):
             tp = c_matrix[i, i]
             fp = c_matrix[:, i].sum() - tp
             fn = c_matrix[i, :].sum() - tp
-            precision = tp / (tp + fp) if tp + fp != 0 else 0
-            recall = tp / (tp + fn) if tp + fn != 0 else 0
-            f1 = 2 * (precision * recall) / (precision + recall) if precision + recall != 0 else 0
+            precision = tp / (tp + fp) if tp + fp > 0 else 0
+            recall = tp / (tp + fn) if tp + fn > 0 else 0
+            f1 = 2 * precision * recall / (precision + recall) if precision + recall > 0 else 0
             results.append({
-            'class': i,
-            'precision': np.round(precision, 3),
-            'recall': np.round(recall, 3),
-            'f1': np.round(f1, 3)
+                'class': i,
+                'precision': precision,
+                'recall': recall,
+                'f1': f1
             })
-            accuracy += tp
-            print(results[i])
-        accuracy /= np.sum(c_matrix)
-        print(f'accuracy: {accuracy}')
-        return results
+        return results, accuracy
